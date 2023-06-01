31384e82d49f431eb6955744831ce994b5f8c209
shayim520 <xayimardan520@gmail.com>
2023-06-01 11:48:07 -0400

    wierAccount practice

---
+++ force-app/main/default/lwc/practice18/practice18.js
@@ -0,0 +1,21 @@
+import { LightningElement, wire } from "lwc";
+import getAccontsIfRevenueLessThan from "@salesforce/apex/AccountController.getAccontsIfRevenueLessThan";
+export default class Practice18 extends LightningElement {
+	
+    sliderValue = 100000;
+
+    columns = [
+        { label: 'Account Name', fieldName: 'Name' },
+        { label: 'Annual Revenue', fieldName: 'AnnualRevenue' },
+      ];
+
+    @wire(getAccontsIfRevenueLessThan, {maxRevenue: '$sliderValue' })
+    myAccount;
+
+
+    handleSliderChange(event){
+        this.sliderValue=event.target.value;
+
+    }
+
+}
