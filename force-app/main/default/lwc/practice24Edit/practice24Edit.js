045149997e0aafd44671d90a2015f72ef7957a51
shayim520 <xayimardan520@gmail.com>
2023-06-05 12:02:11 -0400

    updated Record(@api)

--- force-app/main/default/lwc/practice24Edit/practice24Edit.js
+++ force-app/main/default/lwc/practice24Edit/practice24Edit.js
@@ -1,4 +1,4 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 import OPP_OBJECT from "@salesforce/schema/Opportunity";
 import NAME_FIELD from "@salesforce/schema/Opportunity.Name";
 import STAGE_FIELD from "@salesforce/schema/Opportunity.StageName";
@@ -11,6 +11,14 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class Practice24_edit extends LightningElement {
 
     objectApiName = OPP_OBJECT; 
+
+    //recordId = "006Dn000009HEojIAG";
+
+    // this decoration is used to make this property public
+    // so the parent component or the record page can inject the recordId
+    // to this property automatically if we are on record page
+    @api
+    recordId;
 
     fields = [NAME_FIELD, STAGE_FIELD, CLOSE_DATE_FIELD, AMOUNT_FIELD]; 
 
