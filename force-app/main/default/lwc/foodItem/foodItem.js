d2ee17c6604cf66e1925b18157eba378e6de67f3
shayim520 <xayimardan520@gmail.com>
2023-06-06 16:21:49 -0400

    parent and child componet practice( food menuel )

---
+++ force-app/main/default/lwc/foodItem/foodItem.js
@@ -0,0 +1,12 @@
+import { LightningElement, api } from "lwc";
+export default class FoodItem extends LightningElement {
+
+    // item = {
+    //     foodName: 'Pizza',
+    //     calorie: 2000
+    // }
+    @api
+    menuel;
+
+
+}
