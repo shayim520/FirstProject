b8c229736c4c0ba44f5079ba73459f0a8da1c9ad
shayim520 <xayimardan520@gmail.com>
2023-06-06 15:18:24 -0400

    To The Child practice

--- force-app/main/default/lwc/practice30/practice30.js
+++ force-app/main/default/lwc/practice30/practice30.js
@@ -1,5 +1,13 @@
 import { LightningElement } from "lwc";
 export default class Practice30 extends LightningElement {
 
+    inputMsg;
+
+    handleChange(event){
+        this.inputMsg = event.target.value;
+
+    }
     
+
+
 }
