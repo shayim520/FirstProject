f3b1cdbc29054cd9b7b90fd79932c9c73e692f68
shayim520 <xayimardan520@gmail.com>
2023-06-06 12:56:25 -0400

    parent and child

---
+++ force-app/main/default/lwc/practice30Child/practice30Child.js
@@ -0,0 +1,10 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Practice30Child extends LightningElement {
+
+    //message = 'Hello World';
+    @api  // make this property public , so parent can provide the value
+    message;
+
+
+}
