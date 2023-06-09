cd0e34e43036ff9eabd0895b4eb3ee1d766a9c7c
shayim520 <xayimardan520@gmail.com>
2023-06-09 12:01:00 -0400

    unrelated practice(lmsPublisher and lms Subscriber)

--- force-app/main/default/lwc/lmsSubscriber/lmsSubscriber.js
+++ force-app/main/default/lwc/lmsSubscriber/lmsSubscriber.js
@@ -1,3 +1,44 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+import MY_MESSAGE_CHANNEL from "@salesforce/messageChannel/MyChannel__c";
+import { MessageContext, subscribe, unsubscribe } from "lightning/messageService";
+
+
+
 export default class LmsSubscriber extends LightningElement {
+    
+    receivedMessage = '';
+    
+    @wire(MessageContext)
+    messageContext;
+
+    // create a property to store the subscriotion retured by subscribe method
+
+    subscribe = null;
+    
+   // use lifecycle method connectedCallBack to subscribe to the channel 
+    // so we can get it ready when it's loaded 
+    connectedCallback() {
+        // check if the subscription already exists or not 
+        if (! this.subscription) {
+            // if it is not already exists now call subscribe method 
+            // 1,  MessageContext we wired
+            // 2,  the Message Channel we imported
+            // 3,  the handler function that accept message and take some action
+                    // usually this is arrow function message=> doSomething(message)
+            subscribe(this.messageContext,
+                      MY_MESSAGE_CHANNEL,
+                      message => {
+                          this.receivedMessage = message.myData; 
+                      }
+            )
+    
+        }
+
+
+    }
+
+
+
+
+
 }
